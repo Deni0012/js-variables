3e338a44d778d7c54d3bfbdb56c667913ecde881
Deni0012 <deni.dg00@icloud.com>
2025-02-21 17:02:08 +0100

    feat: completamento esercizio 3

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -31,6 +31,7 @@ console.log(indefinite);
 const valueless = null
 
 console.log(valueless);
+
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
@@ -38,7 +39,7 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + '' + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
